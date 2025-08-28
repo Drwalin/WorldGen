0b09cc26d0070be2e4eff1d64bc4ad4f2788f3e9
Drwalin <34726800+Drwalin@users.noreply.github.com>
2025-08-29 00:33:43 +0200

    Temp commit.

--- tests/gen_3d/fragment.glsl
+++ tests/gen_3d/fragment.glsl
@@ -6,7 +6,6 @@ in vec4 out_color;
 in vec3 pos;
 in vec3 intPos;
 in vec3 normal;
-in vec3 trueNormal;
 in vec3 triangleVert0Pos;
 
 out vec4 FragColor;
@@ -22,7 +21,7 @@ void main()
 
 		if (scale.y * 0.07 > triangleVert0Pos.y) {
 			FragColor = vec4(0.1, 0.1, out_color.x * 0.3 + 0.5, 1);
-		} else if (normal.y < 0.98) {
+		} else if (normal.y < 0.9) {
 			FragColor = vec4(out_color.xyz * 0.3 + 0.4, 1);
 		} else {
 			if (scale.y * 0.085 > triangleVert0Pos.y) {
@@ -35,7 +34,7 @@ void main()
 			}
 		}
 
-		float light = dot(trueNormal, normalize(vec3(-0.6, -0.1, -0.5)));
+		float light = dot(normal, normalize(vec3(-0.6, -0.1, -0.5)));
 // 		light *= light * light;
 		light = light * 0.5 + 0.5;
 		light = light * light;
